85f5de445257e3e3313189a4c708d90bc8bad6d6
x0bit <vrajjazadiya@gmail.com>
2025-08-03 12:50:25 -0700

    Add files via upload

---
+++ DSA/Lab/lab1/Part C/4_a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int arr[] = {10, 20, 30, 40, 50},i;
+
+    cout<<"TRAVERSAL OF AN ARRAY : \n";
+    for(i = 0;i < 5;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
